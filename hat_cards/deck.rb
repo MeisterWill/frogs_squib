ce5a691616766db50b09b93dc858f055149eae46
MeisterWill <meisterwilliam5@gmail.com>
2025-05-01 14:50:21 -0400

    CSV can now be imported and card names are all turned into card names

--- hat_cards/deck.rb
+++ hat_cards/deck.rb
@@ -1,6 +1,11 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
+data = Squib.csv file: 'Hat table - Sheet1.csv'
+
+Squib::Deck.new(cards: data['Name'].size, layout: 'layout.yml') do
+  background color: "white"
+  
+  text str: data['Name']  # data['Name'] is already an array of strings
+  
   save format: :png
 end
